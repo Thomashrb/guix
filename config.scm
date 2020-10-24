cfa7c3ae710239517f121e5db6629fd45c0ac4df
thomas <thomashrb@protonmail.com>
2020-10-25 00:07:26 +0200

    config.scm: configure postgres

--- config.scm
+++ config.scm
@@ -61,7 +61,6 @@
     (specification->package "docker")
     (specification->package "docker-cli")
     (specification->package "flatpak")
-    (specification->package "postgresql")
     (specification->package "elixir")
     (specification->package "erlang")
     (specification->package "rust")
@@ -78,7 +77,16 @@
     (service openssh-service-type)
     (service postgresql-service-type
              (postgresql-configuration
-              (postgresql postgresql-11)))
+              (postgresql postgresql-11)
+              (config-file
+               (postgresql-config-file
+                (hba-file
+                 (plain-file "pg_hba.conf"
+                             "
+local	all	all                 trust
+host	all	all	127.0.0.1/32    trust
+host	all	all	::1/128         md5"))))))
+
     (set-xorg-configuration
      (xorg-configuration
       (keyboard-layout keyboard-layout))))
